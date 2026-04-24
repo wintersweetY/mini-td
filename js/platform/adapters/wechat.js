74f60bbfeec42ca64542d71169dba4dc2fa8b855
yuwuxin <gyyuwuxin@163.com>
2026-04-24 15:43:13 +0800

    fix: 提升微信运行时兼容并同步项目配置

--- js/platform/adapters/wechat.js
+++ js/platform/adapters/wechat.js
@@ -4,7 +4,15 @@ export default class WechatAdapter {
   }
 
   createCanvas() {
-    return this.wx.createCanvas();
+    if (this.wx.createCanvas) {
+      return this.wx.createCanvas();
+    }
+
+    if (typeof canvas !== 'undefined') {
+      return canvas;
+    }
+
+    throw new Error('微信环境未提供 Canvas。');
   }
 
   getWindowInfo() {
@@ -16,11 +24,29 @@ export default class WechatAdapter {
   }
 
   requestAnimationFrame(callback) {
-    return requestAnimationFrame(callback);
+    if (this.wx.requestAnimationFrame) {
+      return this.wx.requestAnimationFrame(callback);
+    }
+
+    if (typeof requestAnimationFrame !== 'undefined') {
+      return requestAnimationFrame(callback);
+    }
+
+    return setTimeout(callback, 16);
   }
 
   cancelAnimationFrame(id) {
-    cancelAnimationFrame(id);
+    if (this.wx.cancelAnimationFrame) {
+      this.wx.cancelAnimationFrame(id);
+      return;
+    }
+
+    if (typeof cancelAnimationFrame !== 'undefined') {
+      cancelAnimationFrame(id);
+      return;
+    }
+
+    clearTimeout(id);
   }
 
   now() {
--- js/render/canvas.js
+++ js/render/canvas.js
@@ -1,10 +1,18 @@
 export function initCanvas(platform) {
   const localCanvas = platform.createCanvas();
   const windowInfo = platform.getWindowInfo();
+  const runtimeGlobal =
+    typeof globalThis !== 'undefined'
+      ? globalThis
+      : typeof window !== 'undefined'
+        ? window
+        : typeof global !== 'undefined'
+          ? global
+          : {};
   const globalScope =
     typeof GameGlobal !== 'undefined'
       ? GameGlobal
-      : (globalThis.GameGlobal = {});
+      : (runtimeGlobal.GameGlobal = {});
 
   localCanvas.width = windowInfo.screenWidth;
   localCanvas.height = windowInfo.screenHeight;
