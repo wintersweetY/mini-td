79619714d9fa99b4af66afd2cd0406a4cf9c7901
yuwuxin <gyyuwuxin@163.com>
2026-04-24 15:49:49 +0800

    fix: 显式引用平台入口以兼容微信模块解析

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-import { createPlatformBridge } from './platform';
+import { createPlatformBridge } from './platform/index';
 import { initCanvas } from './render/canvas';
 import SceneRenderer from './render/scene-renderer';
 import Game from './core/game';
+import { FIXED_TIME_STEP_MS, MAX_FRAME_DELTA_MS } from './runtime/constants';
 
 export default class Main {
   constructor() {
@@ -19,17 +20,36 @@ export default class Main {
       height: this.canvas.height
     });
 
+    this.lastTime = 0;
+    this.accumulator = 0;
     this.loop = this.loop.bind(this);
+
     this.start();
   }
 
   start() {
     this.game.start();
+    this.lastTime = 0;
+    this.accumulator = 0;
     this.aniId = this.platform.requestAnimationFrame(this.loop);
   }
 
   loop() {
-    this.game.update();
+    const now = this.platform.now();
+
+    if (this.lastTime === 0) {
+      this.lastTime = now;
+    }
+
+    const deltaMs = Math.min(now - this.lastTime, MAX_FRAME_DELTA_MS);
+    this.lastTime = now;
+    this.accumulator += deltaMs;
+
+    while (this.accumulator >= FIXED_TIME_STEP_MS) {
+      this.game.update(FIXED_TIME_STEP_MS / 1000);
+      this.accumulator -= FIXED_TIME_STEP_MS;
+    }
+
     this.renderer.render(this.ctx, this.game.getSnapshot());
     this.aniId = this.platform.requestAnimationFrame(this.loop);
   }
